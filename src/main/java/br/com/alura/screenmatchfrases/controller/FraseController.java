64e5429e04f904b46467094c57ccd1799bbb1242
Rafael Amorim <rafaelamorim1189@gmail.com>
2024-04-17 17:15:40 -0300

    Applicação completa

---
+++ src/main/java/br/com/alura/screenmatchfrases/controller/FraseController.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatchfrases.controller;
+
+import br.com.alura.screenmatchfrases.dto.FraseDTO;
+import br.com.alura.screenmatchfrases.service.FraseServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FraseController {
+
+    @Autowired
+    private FraseServico servico;
+
+
+    @GetMapping("/series/frases")
+    public FraseDTO obterFraseAleatoria(){
+        return servico.obterFraseAleatoria();
+    }
+
+}
